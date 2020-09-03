64345aba80f163a17dcb998c2e7e4a9a71059cbb
Sumit Dhungana <sumeetdhungana74@gmail.com>
2020-09-03 17:47:58 +0545

    Add preferred orientation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:expence_tracker/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './home/my_home_page.dart';
 
-void main() => runApp(MaterialApp(
-      home: MyHomePage(),
-      title: 'Expense Tracker',
-      theme: AppTheme.getAppTheme(),
-    ));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  return runApp(MaterialApp(
+    home: MyHomePage(),
+    title: 'Expense Tracker',
+    theme: AppTheme.getAppTheme(),
+  ));
+}
